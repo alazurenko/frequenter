d1708acef7e79b4a82b4434dd588f10af22ef8de
Lazurenko, Andrii <Andrii_Lazurenko@epam.com>
2019-04-19 11:57:12 +0200

    add on hover indication of progress

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ const date = new Date();
 const month = date.getMonth() + 1;
 const daysInMonth = date.getDate();
 const passedDays = 31 * (month - 1) + daysInMonth;
-const progress = passedDays / p;
+const progress = (passedDays / p).toFixed(2);
 // 
 
 
 bar.style.setProperty("--progress", `${progress}%`);
+bar.style.setProperty("--progress-pass-content", `'${progress} %'`);
+bar.style.setProperty("--progress-upcoming-content", `'${100 - progress} %'`);
