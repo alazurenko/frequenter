467280eba97eabcb4e4fc0407be544d78d6f5280
Andrii Lazurenko <andrey.lazurenko@gmail.com>
2021-09-09 22:44:12 +0300

    chore: async sample

--- components/authenitcation.tsx
+++ components/authenitcation.tsx
@@ -1,11 +1,11 @@
-import { useAuthentication } from "./authenitcation.hook";
 import { useStoreon } from "storeon/react";
-import { AuthSetAuthEvent } from "./../store/auth.module";
+import { AuthSetAuthEvent, AuthLoadEvent } from "./../store/auth.module";
 import { State, Events } from "../store";
 
 export const Authentication: React.FC = () => {
   const { auth, dispatch } = useStoreon<State, Events>("auth");
   const buttonText = auth.isAuthenticated ? "Log out" : "Log in";
+  const loadingText = auth.isLoading ? "Loading pelase wait" : "";
 
   const LogInButton = () => (
     <button
@@ -24,10 +24,21 @@ export const Authentication: React.FC = () => {
     </button>
   );
 
+  const LoadButton = () => (
+    <button
+      className="bg-black hover:bg-gray-600 text-white py-1 px-2 rounded mr-2"
+      onClick={() => dispatch(AuthLoadEvent)}
+    >
+      load
+    </button>
+  );
+
   return (
     <div>
       <p>You're {auth.isAuthenticated ? "logged in" : "not logged in"}</p>
+      <p>{auth.isAuthenticated ? loadingText : ''}</p>
       {auth.isAuthenticated ? <LogOutButton /> : <LogInButton />}
+      {auth.isAuthenticated ? <LoadButton /> : ''}
     </div>
   );
 };
--- store/auth.module.ts
+++ store/auth.module.ts
@@ -7,17 +7,25 @@ const NAME = "auth";
 export interface AuthModuleState {
   [NAME]: {
     isAuthenticated: boolean;
+    isLoading: boolean,
   };
 }
 
 const AUTH_DEFAULT_STATE = {
   isAuthenticated: false,
+  isLoading: false,
 };
 
 export const AuthSetAuthEvent = `auth/setAuthenitcated`;
+export const AuthLoadEvent = `auth/load`;
+export const AuthSetLoadingStartEvent = `auth/setLoadingStart`;
+export const AuthSetLoadingEndEvent = `auth/setLoadingEnd`;
 
 export interface AuthModuleEvents {
   [AuthSetAuthEvent]: boolean;
+  [AuthLoadEvent],
+  [AuthSetLoadingStartEvent],
+  [AuthSetLoadingEndEvent],
 }
 
 export const authModule = (parentStore: StoreonStore<State, Events>) => {
@@ -29,4 +37,14 @@ export const authModule = (parentStore: StoreonStore<State, Events>) => {
       isAuthenticated,
     };
   });
+
+  store.on(AuthLoadEvent, () => {
+    store.dispatch(AuthSetLoadingStartEvent);
+    setTimeout(() => {
+      store.dispatch(AuthSetLoadingEndEvent);
+    }, 2000);
+  })
+
+  store.on(AuthSetLoadingStartEvent, (store) => ({...store, isLoading: true}));
+  store.on(AuthSetLoadingEndEvent, (store) => ({...store, isLoading: false}));
 };
